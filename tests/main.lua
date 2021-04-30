b6304070cec53e846016f44adf8233e3635dff61
DedlySpyder <dedlyspyder@gmail.com>
2021-04-30 01:30:09 -0400

    Fixed up main test to run area tests

--- tests/main.lua
+++ tests/main.lua
@@ -1,11 +1,17 @@
 -- TODO - this can all be a new mod?
+local Tester = require("modules/tester")
 
-local logger = require("logger")
+local logger = require("tests/logger")
+local area = require("tests/area")
 
 return function()
     logger()
+
+    Tester.add_tests(area, "Area")
+    Tester.run()
 end
 
+--TODO - this test will mess up other tests, so when these are all moved out of the main lib needs to be mutex with other tests
 --local tester = require("tester")
 --return function()
 --    tester()
