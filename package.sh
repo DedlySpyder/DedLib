87a5100031c4d413f7b773602209462cfb2ef68d
DedlySpyder <dedlyspyder@gmail.com>
2021-06-05 02:24:47 -0400

    Fixed package script

--- package.sh
+++ package.sh
@@ -4,9 +4,12 @@ set -e
 # https://stedolan.github.io/jq/
 mod_name=$(jq -r '.name' info.json)
 mod_version=$(jq -r '.version' info.json)
+mod_name_and_version="${mod_name}_${mod_version}"
 
 # Create git tag for this version
 git tag "$mod_version"
 
 # Create zip with exclusions
-zip -r "${mod_name}_${mod_version}.zip" . -x '*.git*' 'tests/*' 'package.sh' 'docs/*' '*.iml'
+cd ..
+zip -r "${mod_name_and_version}.zip" "${mod_name_and_version}" -x '*.git*' 'tests/*' 'package.sh' 'docs/*' '*.iml'
+cd -
