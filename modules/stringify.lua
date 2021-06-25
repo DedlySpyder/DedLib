eb1ec1bce3198be4bc82c0a56d533c3d93731531
DedlySpyder <dedlyspyder@gmail.com>
2021-06-25 01:51:59 -0400

    Refactored formatters to be DRYer

--- modules/stringify.lua
+++ modules/stringify.lua
@@ -19,113 +19,81 @@ Stringify._ALWAYS_VALID = {
 
 
 Stringify.Formatters = {}
+function Stringify.Formatters._run_formatter(funcName, arg)
+    local format, values = Stringify.Formatters[funcName](arg)
+    return string.format("<%s>{" .. format .. "}", funcName, table.unpack(values or {}))
+end
+
 function Stringify.Formatters.LuaCustomTable(arg)
-    return string.format("<LuaCustomTable>{%d item(s)}", #arg)
+    return "%d item(s)", {#arg}
 end
 
 function Stringify.Formatters.LuaEntity(arg)
     local pos = arg.position
-    return string.format("<LuaEntity>{type=%s, name=%s, unit_number=%s, position={%s,%s}}",
-            arg.type,
-            arg.name,
-            tostring(arg.unit_number),
-            tostring(pos.x),
-            tostring(pos.y)
-    )
+    return "type=%s, name=%s, unit_number=%s, position={%s,%s}", {
+        arg.type,
+        arg.name,
+        tostring(arg.unit_number),
+        tostring(pos.x),
+        tostring(pos.y)
+    }
 end
 
 function Stringify.Formatters.LuaEntityPrototype(arg)
-    return string.format("<LuaEntityPrototype>{type=%s, name=%s}",
-            arg.type,
-            arg.name
-    )
+    return "type=%s, name=%s", {arg.type, arg.name}
 end
 
 function Stringify.Formatters.LuaForce(arg)
-    return string.format("<LuaForce>{name=%s, player_count=%s}",
-            arg.name,
-            tostring(#arg.players)
-    )
+    return "name=%s, player_count=%s", {arg.name, tostring(#arg.players)}
 end
 
 function Stringify.Formatters.LuaGuiElement(arg)
-    return string.format("<LuaGuiElement>{player_index=%s, name=%s, type=%d, index=%s}",
-            arg.player_index,
-            arg.name,
-            arg.type,
-            tostring(#arg.index)
-    )
+    return "player_index=%s, name=%s, type=%d, index=%s", {arg.player_index, arg.name, arg.type, tostring(#arg.index)}
 end
 
 function Stringify.Formatters.LuaItemPrototype(arg)
-    return string.format("<LuaItemPrototype>{type=%s, name=%s}",
-            arg.type,
-            arg.name
-    )
+    return "type=%s, name=%s", {arg.type, arg.name}
 end
 
 function Stringify.Formatters.LuaItemStack(arg)
     if arg.valid_for_read then
-        return string.format("<LuaItemStack>{type=%s, name=%s, count=%s}",
-                arg.type,
-                arg.name
-        )
+        return "type=%s, name=%s, count=%s", {arg.type, arg.name}
     else
-        return "<LuaItemStack{invalid for reading}>"
+        return "invalid for reading"
     end
 end
 
 function Stringify.Formatters.LuaPlayer(arg)
-    return string.format("<LuaPlayer>{name=%s, index=%s}",
-            arg.name,
-            arg.index
-    )
+    return "name=%s, index=%s", {arg.name, arg.index}
 end
 
 function Stringify.Formatters.LuaRecipe(arg)
-    return string.format("<LuaRecipe>{name=%s}",
-            arg.name
-    )
+    return "name=%s", {arg.name}
 end
 
 function Stringify.Formatters.LuaRecipePrototype(arg)
-    return string.format("<LuaRecipePrototype>{name=%s}",
-            arg.name
-    )
+    return "name=%s", {arg.name}
 end
 
 function Stringify.Formatters.LuaSurface(arg)
-    return string.format("<LuaSurface>{name=%s, index=%s}",
-            arg.name,
-            arg.index
-    )
+    return "name=%s, index=%s", {arg.name, arg.index}
 end
 
 function Stringify.Formatters.LuaTechnology(arg)
-    return string.format("<LuaTechnology>{name=%s}",
-            arg.name
-    )
+    return "name=%s", {arg.name}
 end
 
 function Stringify.Formatters.LuaTechnologyPrototype(arg)
-    return string.format("<LuaTechnologyPrototype>{name=%s}",
-            arg.name
-    )
+    return "name=%s", {arg.name}
 end
 
 function Stringify.Formatters.LuaTile(arg)
     local pos = arg.position
-    return string.format("<LuaTile>{name=%s, position={%s,%s}}",
-            arg.name,
-            tostring(pos.x),
-            tostring(pos.y)
-    )
+    return "name=%s, position={%s,%s}", {arg.name, tostring(pos.x), tostring(pos.y)}
 end
 
 function Stringify.Formatters.LuaTilePrototype(arg)
-    return string.format("<LuaTilePrototype>{name=%s}",
-            arg.name
-    )
+    return "name=%s", {arg.name}
 end
 
 
@@ -143,9 +111,8 @@ function Stringify.to_string(arg, block, notFirst)
         if Stringify.is_lua_object(arg) then
             local oName = arg.object_name
             if Stringify._ALWAYS_VALID[oName] or arg.valid then
-                local formatter = Stringify.Formatters[oName]
-                if formatter then
-                    local success, result = pcall(formatter, arg)
+                if Stringify.Formatters[oName] then
+                    local success, result = pcall(Stringify.Formatters._run_formatter, oName, arg)
                     if success then
                         return result
                     else
