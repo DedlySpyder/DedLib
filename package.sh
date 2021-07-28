177921ea3e9180ab03ebeff1c151522d465f425d
DedlySpyder <dedlyspyder@gmail.com>
2021-07-27 20:38:10 -0400

    Fixed package script for version agnostic mod dir name

--- package.sh
+++ package.sh
@@ -5,11 +5,12 @@ set -e
 mod_name=$(jq -r '.name' info.json)
 mod_version=$(jq -r '.version' info.json)
 mod_name_and_version="${mod_name}_${mod_version}"
+mod_dir="$(basename "$(readlink -f "$(dirname "$0")")")"
 
 # Create git tag for this version
 git tag "$mod_version"
 
 # Create zip with exclusions
 cd ..
-zip -r "${mod_name_and_version}.zip" "${mod_name_and_version}" -x '*/*.git*' '*/package.sh' '*/docs/*' '*/*.iml'
+zip -r "${mod_name_and_version}.zip" "${mod_dir}" -x '*/*.git*' '*/package.sh' '*/docs/*' '*/*.iml'
 cd -
