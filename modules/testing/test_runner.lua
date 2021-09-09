42806334c46f96d2d43085413ae04af501b35ace
DedlySpyder <dedlyspyder@gmail.com>
2021-09-08 22:14:00 -0400

    Added external result counts to Test_Runner

--- modules/testing/test_runner.lua
+++ modules/testing/test_runner.lua
@@ -7,6 +7,7 @@ Test_Runner.__which = "Test_Runner"
 
 Test_Runner.ALL_TEST_GROUPS = {}
 Test_Runner.ALL_TEST_GROUPS_COUNTS = {}
+Test_Runner.EXTERNAL_RESULT_COUNTS = {}
 
 
 -- Name first, unless the only arg is the test, then continue with an unnamed test
@@ -23,6 +24,17 @@ function Test_Runner.add_test_group(testGroup)
     table.insert(Test_Runner.ALL_TEST_GROUPS.incomplete, testGroup)
 end
 
+function Test_Runner.add_external_results(resultCounts)
+    if type(resultCounts) == "table" then
+        Logger:trace("Adding external results: %s", resultCounts)
+        Test_Runner.EXTERNAL_RESULT_COUNTS.skipped = Test_Runner.EXTERNAL_RESULT_COUNTS.skipped + (resultCounts.skipped or 0)
+        Test_Runner.EXTERNAL_RESULT_COUNTS.failed = Test_Runner.EXTERNAL_RESULT_COUNTS.failed + (resultCounts.failed or 0)
+        Test_Runner.EXTERNAL_RESULT_COUNTS.succeeded = Test_Runner.EXTERNAL_RESULT_COUNTS.succeeded + (resultCounts.succeeded or 0)
+    else
+        Logger:warn("Invalid external results: %s", resultCounts)
+    end
+end
+
 function Test_Runner.reset()
     Logger:trace("Resetting test groups")
     Test_Runner.ALL_TEST_GROUPS = {
@@ -32,6 +44,12 @@ function Test_Runner.reset()
     }
 
     Test_Runner.ALL_TEST_GROUPS_COUNTS = {
+        skipped = 0,
+        failed = 0,
+        succeeded = 0
+    }
+
+    Test_Runner.EXTERNAL_RESULT_COUNTS = {
         skipped = 0,
         failed = 0,
         succeeded = 0
