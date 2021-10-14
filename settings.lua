770c1a70d568fdd58ffe04ae7f7340d82829879f
DedlySpyder <dedlyspyder@gmail.com>
2021-10-13 21:57:36 -0400

    Reordered old settings in file

--- settings.lua
+++ settings.lua
@@ -1,19 +1,21 @@
 data:extend({
 	-- Startup settings
 	{
-		name = "DedLib_logger_level_console",
-		type = "string-setting",
-		setting_type = "runtime-global",
-		default_value = "off",
-		allowed_values = {"off", "fatal", "error", "warn", "info", "debug", "trace"},
-		order = "900"
-	},
-	{
 		name = "DedLib_logger_level_file",
 		type = "string-setting",
 		setting_type = "startup",
 		default_value = "error",
 		allowed_values = {"off", "fatal", "error", "warn", "info", "debug", "trace"},
 		order = "910"
+	},
+
+	-- Runtime settings
+	{
+		name = "DedLib_logger_level_console",
+		type = "string-setting",
+		setting_type = "runtime-global",
+		default_value = "off",
+		allowed_values = {"off", "fatal", "error", "warn", "info", "debug", "trace"},
+		order = "900"
 	}
 })
